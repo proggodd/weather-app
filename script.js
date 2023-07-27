e445cd8572b70ff2b6742f114233c9cd5bf8ef35
ibrahim khaled <41508525+proggodd@users.noreply.github.com>
2023-07-27 20:54:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const apiKey = "fdcd786914865d86a6dbb8aec22b98c9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector("input");
+const searchBtn = document.querySelector("button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status == 404)
+    {
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "block";
+    } else {
+        var data = await response.json();
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        document.querySelector(".weather-icon").src = "images/" + data.weather[0].main + ".png"
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+        
+    }
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+
+})
